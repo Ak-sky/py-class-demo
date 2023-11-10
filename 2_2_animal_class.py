d0707702b8de6eaa87056e8960e0e3195dc46fbe
Akash Kumar <akash.kumar@ibm.com>
2023-11-11 01:30:37 +0530

    python demo

--- 2_2_animal_class.py
+++ 2_2_animal_class.py
@@ -4,15 +4,4 @@ class Cat(Animal):
         super().__init__(name, age)
         self.color = color
 
-    def description(self):
-        print("======= Description of the Animal(Cat) =======")
-        return f"Name: {self.name}\nAge: {self.age}\nColor: {self.color}\n"
-
-    def speak(self, sound):
-        print(super().speak(sound))
-        return f"{self.name} meow: {sound}"
-
-    def get_speed(self, speed):
-        return f"The speed of the cats are generally {speed} km/h"
-
 
