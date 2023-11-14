a35db44cae0623372949ff734c4f1a5b4d1b7306
Akash Kumar <akash.kumar@ibm.com>
2023-11-15 02:15:52 +0530

    python demo

--- 2_2_animal_class.py
+++ 2_2_animal_class.py
@@ -1,7 +1,19 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
-class Cat(Animal):
-    def __init__(self, name, age, color):
-        super().__init__(name, age)
-        self.color = color
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+    def get_detail(self):
+        return f"Length: {self.length}, Width: {self.width}"
 
 
+rect1 = Rectangle(5, 4)
+print(rect1.area())
+print(rect1.perimeter())
+print(rect1.get_detail())
